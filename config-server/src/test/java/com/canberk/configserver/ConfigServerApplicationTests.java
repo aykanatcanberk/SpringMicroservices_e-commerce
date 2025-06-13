c3011944b80feea16f7eb19a657afc47b80f6c73
Canberk Aykanat <kayra.aykanat@gmail.com>
2025-06-13 16:37:08 +0300

    config server and discovery

---
+++ config-server/src/test/java/com/canberk/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canberk.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
